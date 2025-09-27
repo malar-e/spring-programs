bbd5cbd3d58d4f2570795654b5ec04a685c640ba
Malarvizhi E <malarvizhi.e@gendigital.com>
2025-09-27 17:47:53 +0530

    Add CarConfig

---
+++ spring-gradle-demo/app/src/main/java/car/CarConfiguration.java
@@ -0,0 +1,10 @@
+package car;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configuration
+@ComponentScan("car")
+public class CarConfiguration {
+    
+}
